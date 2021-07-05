c849e97b3bbca8ea42afc8e003541f65c1b64c48
HOME-PC\Stelios <steliosdirects@gmail.com>
2021-07-05 13:11:07 +0300

    Initial Commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Stelios Alexandrakis <br/>
+        Junior Web Developer <br/>
+        Portfolio
+      </SectionTitle>
+      <SectionText>
+        The purpose of Javascript mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps. 
+      </SectionText>
+      <Button>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
